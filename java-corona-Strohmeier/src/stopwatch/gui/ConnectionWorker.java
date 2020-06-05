43c920b1aeeb5fe87e8ffdd4927bbb1641d8ba22
strjum16 <strjum16@htlkaindord.at>
2020-06-05 12:04:27 +0200

    GUI angelegt, ConnectionWorker angelegt

---
+++ java-corona-Strohmeier/src/stopwatch/gui/ConnectionWorker.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stopwatch.gui;
+
+import java.util.List;
+import javax.swing.SwingWorker;
+
+/**
+ *
+ * @author JulianFew
+ */
+public class ConnectionWorker extends SwingWorker {
+
+    
+    
+    @Override
+    protected void done() {
+        super.done(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void process(List chunks) {
+        super.process(chunks); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected Object doInBackground() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
